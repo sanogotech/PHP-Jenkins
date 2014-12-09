2871f91365ed4aae9b45e1b562dc67d976c70a6c
Hiren Bhatt <hirenbh@cybage.com>
2014-12-09 18:39:14 +0530

    Added Index.php code

--- src/index.php
+++ src/index.php
@@ -1,8 +1,173 @@
-<?php
-echo " Hello ALL";
-echo " Deployed First PHP Application";
-echo "--------------------------------";
-echo phpinfo();
-echo "--------------Finish-----------";
+<html>
+<head>
+    <meta charset="utf-8">
+    
+    <title><?php echo "First website Deployment | PHP";?></title>
+    <meta name="description" content="Sweet and simple, this free website template awaits your bakery, café, or catering company. Upload images to the photo gallery to display your goods in mouth-watering detail. Play with color and add text to build a website as unique as your tasty creations.">
+    <meta name="author" content="Wixpress">
+    <meta http-equiv="content-language" content="en"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 
-?>
+    <meta property="og:title" content="Cupcake Shop Website Template | WIX"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="http://www.wix.com/website-template/view/html/651"/>
+    <meta property="og:image" content="http://static.wix.com/media/541b84f9d748cc0640e4c6fd1d154d4d.wix_mp"/>
+    <meta content="Wix" property="og:site_name">
+    <meta property="og:description" content="Sweet and simple, this free website template awaits your bakery, café, or catering company. Upload images to the photo gallery to display your goods in mouth-watering detail. Play with color and add text to build a website as unique as your tasty creations."/>
+    <meta property="fb:admins" content="731184828"/>
+    <meta name="fb:app_id" content="236335823061286"/>
+    <meta name="google-site-verification" content="QXhlrY-V2PWOmnGUb8no0L-fKzG48uJ5ozW0ukU7Rpo"/>
+
+        <link rel="canonical" href="http://www.wix.com/website-template/view/html/651"/>
+
+            <link rel="alternate" hreflang="fr" href="http://fr.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="pt" href="http://pt.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="it" href="http://it.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="ko" href="http://ko.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="de" href="http://de.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="ru" href="http://ru.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="tr" href="http://tr.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="en" href="http://www.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="es" href="http://es.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="ja" href="http://ja.wix.com/website-template/view/html/651"/>
+            <link rel="alternate" hreflang="pl" href="http://pl.wix.com/website-template/view/html/651"/>
+        <link rel="alternate" hreflang="x-default" href="http://www.wix.com/website-template/view/html/651" />
+
+
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.598.0/css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.598.0/new-create/css/fonts.css"/>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-public/1.144.0/css/Helvetica/fontFace.css"/>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.598.0/css/template_viewer.css"/>
+
+
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.598.0/css/ie9-and-down.css"/>
+    <![endif]-->
+
+    </head>
+<body lang="en">
+<!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MDD5C4"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+            {'gtm.start': new Date().getTime(),event:'gtm.js'}
+    );var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MDD5C4');</script>
+<!-- End Google Tag Manager -->
+    
+<script type="text/javascript">
+    var backUrl = "http:\/\/www.wix.com\/website\/templates";
+    var editUrl = "http://editor.wix.com/html/editor/web/renderer/new?metaSiteId=82079a7a-3242-4e04-a45a-6ee8a766ba2e&siteId=21109cc5-28ed-4ef1-9856-67a8aaa461b6";
+    var galleryDocIndex = "$pageModel.galleryDocIndex";
+    var originAppUrl = "$pageModel.unsafeJSoriginAppUrl";
+    var W = W || {};
+    W.App = W.App || {};
+    W.App.CLIENT_CONFIG = {"biServerUrl":"http://frog.wix.com/spettatore_bi?src=2&openingSiteId=21109cc5-28ed-4ef1-9856-67a8aaa461b6&originAppUrl=http%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates&siteType=2"};
+    W.App.currentViewMode = 'desktop';
+
+    W.Globals = W.Globals || {};
+    W.Globals.lang =  "en";
+    var analytics = {"staging":false,"code":"UA-2117194-1","domain":"wix.com"};
+    var openAsTab = true;
+
+    //Storing dynamic text for responsive design
+    var headerTxt = {};
+    headerTxt.editButtonSmall = 'Edit';
+    headerTxt.mainHeaderSmall = 'Create your own amazing website';
+</script>
+
+<!-- FACEBOOK CODE API-->
+<div id="fb-root"></div>
+<script>
+    /* Facebook Like Button*/
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    /*Google+ */
+    (function () {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+</script>
+<!-- End of FACEBOOK CODE -->
+
+<div id="container" class="template_viewer">
+  
+    <div id="headerShadows">
+        <div class="rightShadow"></div>
+        <div class="leftShadow"></div>
+    </div>
+    <!-- End of #HeaderShadows-->
+    <div class="contentWrapper">
+                                    <div class="preview-tab preview-tab-desktop">
+                <iframe src="http://www.wix.com/demone2/sugar-and-spice" width="100%" height="100%"></iframe>
+            </div>
+        
+        <!--</div>--><!-- End of .content-->
+    </div>
+    <!-- End of .contentWrapper-->
+
+    <div id="dialog-overlay"></div>
+    <div id="dialog-box">
+        <a id="close-pop-up-button" href="#"><?php echo "Close Pop Up Button";?></a>
+        <div class="dialog-content">
+            <h1 class="dialog-title">&#8216;Cupcake Shop&#8217; - Website Template</h1>
+            <div class="leftColumn">
+                <h3>Good For:</h3>
+
+                <div id="dialog-good-for">Retailers, Shops and Product Based Businesses</div>
+                <h3>Description:</h3>
+
+                <div id="dialog-description" itemprop="description">Sweet and simple, this free website template awaits your bakery, café, or catering company. Upload images to the photo gallery to display your goods in mouth-watering detail. Play with color and add text to build a website as unique as your tasty creations.</div>
+            </div>
+            <div class="rightColumn">
+                                                                <span>
+					<h3>Difficulty:&nbsp;</h3>
+					<div id="dialog-difficulty" itemprop="difficulty">Easy</div>
+				</span>
+				<span itemprop="author" itemscope itemtype="http://schema.org/Organization">
+					<h3>Author:&nbsp;</h3>
+					<div id="dialog-author" itemprop="name">Wix Studio</div>
+				</span>
+				<span itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+					<h3>Price:&nbsp;</h3>
+					<div id="dialog-price" itemprop="price">Free</div>
+					<link itemprop="availability" href="http://schema.org/InStock"/>
+				</span>
+                <a href="#" id="dialog-edit-now">Edit now</a>
+            </div>
+            <div class="clearBoth">&nbsp;</div>
+        </div>
+        <div class="dialog-box-shadows">
+            <div class="leftShadow"></div>
+            <div class="rightShadow"></div>
+        </div>
+    </div>
+    <!-- End of #dialog-box-->
+
+</div>
+<!-- End of #container-->
+
+<!--Scripts after page loads-->
+<script src="http://static.wix.com/services/third-party/jquery/1.4.3/min/jquery.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://static.parastorage.com/services/wix-html-template-viewer/1.598.0/js/components/scrollbar/custom-scrollbar.js"></script>
+<script type="text/javascript" src="http://static.parastorage.com/services/wix-html-template-viewer/1.598.0/js/common.js"></script>
+<script src="http://static.parastorage.com/services/wix-html-template-viewer/1.598.0/js/template_viewer.js" type="text/javascript"></script>
+
+        
+
+</body>
+</html>
